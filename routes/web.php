c56ae3b5586bac8ec2593320b50120ff0557fdb1
Levi Costa <levi.costa1@gmail.com>
2022-04-12 10:38:33 -0300

    fix auth routes

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,20 @@ Route::get('/', [\App\Http\Controllers\IndexController::class, 'index']);
 Route::get('/api/coin/price', [\App\Http\Controllers\CoinController::class, 'index'])->name('api.coin.convert');
 
 //Auth::routes();
+// Authentication Routes...
+$this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
+$this->post('login', 'Auth\LoginController@login');
+$this->post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Registration Routes...
+$this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+$this->post('register', 'Auth\RegisterController@register');
+
+// Password Reset Routes...
+$this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm');
+$this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
+$this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm');
+$this->post('password/reset', 'Auth\ResetPasswordController@reset');
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/buy/{poke_id}', [App\Http\Controllers\BuyController::class, 'buy'])->name('buy');
